bac7df258b8a07ec7dd53e20c9caae7af41e158b
Kunal <kunal@Kunals-MacBook-Air.local>
2025-07-31 11:02:53 +0530

    add multiple browsers

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,26 @@
 module.exports = {
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    }
+  ],
+  reporter: [['html'], ['allure-playwright']],
   testDir: './tests',
   retries: 0,
   timeout: 30000,
-  reporter: [['html'], ['allure-playwright']],
   use: {
     headless: true,
-    viewport: { width: 1280, height: 720 },
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
+    video: 'retain-on-failure',
+    viewport: { width: 1280, height: 720 }
   }
 };
