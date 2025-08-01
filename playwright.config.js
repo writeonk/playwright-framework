a4da509cd1441bcb38b4be7bebfc876d49273586
Kunal <kunal@Kunals-MacBook-Air.local>
2025-08-01 19:18:33 +0530

    changes in config for allure-report-github actions

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,20 @@
-module.exports = {
+// playwright.config.js
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  retries: 0,
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    viewport: { width: 1280, height: 720 }
+  },
+  reporter: [
+    ['list'], // or 'html' for local debugging
+    ['allure-playwright', { outputFolder: 'allure-results' }]
+  ],
   projects: [
     {
       name: 'Chromium',
@@ -13,14 +29,4 @@ module.exports = {
       use: { browserName: 'webkit' },
     }
   ],
-  reporter: [['html'], ['allure-playwright']],
-  testDir: './tests',
-  retries: 0,
-  timeout: 30000,
-  use: {
-    headless: true,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    viewport: { width: 1280, height: 720 }
-  }
-};
+});
